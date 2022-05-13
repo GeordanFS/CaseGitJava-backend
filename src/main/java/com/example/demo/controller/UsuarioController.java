dc703099d95cdb07c46dbcf0fee30a7d14aa4a99
GeordanFS <geordansantos@gmail.com>
2022-05-13 18:30:15 -0300

    Alteração no pom

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -25,7 +25,7 @@ import com.example.demo.model.Usuario;
 
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin("*")
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class UsuarioController {
 
 	@Autowired
